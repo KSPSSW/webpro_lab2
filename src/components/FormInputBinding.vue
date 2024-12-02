41afad012525019ebfa715bd1a0fc98133b8a5b9
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 21:10:12 +0700

    Array CheckBox

--- src/components/FormInputBinding.vue
+++ src/components/FormInputBinding.vue
@@ -1,11 +1,31 @@
 <template>
   <div>From Input Binding</div>
   <div>{{ text }}</div>
-  <input type="text" :value="text" @input="(e) => (text = e.target.value)" />
+  <input v-model="text" />
+
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line">{{ message }}</p>
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const text = ref('Hello')
-// const inputText = (e) => (text.value = e.target.value)
+const message = ref('')
+const checked = ref(true)
+const checkedNames = ref([])
 </script>
