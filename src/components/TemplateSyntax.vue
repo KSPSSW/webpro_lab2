ee3e0343437cedb00774583065e4549cb4437e65
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 20:17:43 +0700

    complete Binding 1

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,9 +1,13 @@
 <script setup>
 var msg = 'Hello Message'
 var rawHtml = '<h1>Hello</h1>'
+var myId = 'myId1'
+var color = 'blue'
 </script>
 
 <template>
   <div>{{ msg }}</div>
   <div v-html="rawHtml"></div>
+  <div :id="myId"></div>
+  <font :color="color">Red</font>
 </template>
