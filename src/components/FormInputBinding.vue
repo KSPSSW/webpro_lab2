a79eed18fcf8877afeb4a0a5f55e7581c9f41b79
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 20:59:27 +0700

    arrow function

---
+++ src/components/FormInputBinding.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>From Input Binding</div>
+  <div>{{ text }}</div>
+  <input type="text" :value="text" @input="inputText" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('Hello')
+const inputText = (e) => {
+  console.log(e.target.value)
+  text.value = e.target.value
+}
+</script>
