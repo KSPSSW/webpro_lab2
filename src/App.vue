bbb7f52cfe96c3f3128c604fdfbf969e550e7c52
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 21:24:05 +0700

    HelloMyName

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@ import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import ReactiveFundamentals from './components/ReactiveFundamentals.vue'
 import FormInputBinding from './components/FormInputBinding.vue'
+import HelloMyName from './components/HelloMyName.vue'
+import MyPlus from './components/MyPlus.vue'
 </script>
 
 <template>
+  <MyPlus></MyPlus>
+  <hr />
+  <HelloMyName></HelloMyName>
+  <hr />
   <FormInputBinding></FormInputBinding>
   <hr />
   <ReactiveFundamentals></ReactiveFundamentals>
---
+++ src/components/MyPlus.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <label for="num1">num1</label>
+    <input type="text" id="num1" v-model="num1" />
+    <label for="num2">num2</label>
+    <input type="text" id="num2" v-model="num2" />
+    =
+    {{ num1 + num2 }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const num1 = ref(0)
+const num2 = ref(0)
+</script>
