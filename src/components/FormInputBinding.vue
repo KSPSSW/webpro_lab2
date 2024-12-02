7b03c3da4418898c7747535f35c9e18179fc268b
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 21:15:16 +0700

    Selected

--- src/components/FormInputBinding.vue
+++ src/components/FormInputBinding.vue
@@ -20,6 +20,31 @@
 
   <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
   <label for="mike">Mike</label>
+
+  <div>Picked: {{ picked }}</div>
+
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
+
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
+
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+
+  <div>Selected: {{ selected2 }}</div>
+
+  <select v-model="selected2" multiple>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
 </template>
 
 <script setup>
@@ -28,4 +53,7 @@ const text = ref('Hello')
 const message = ref('')
 const checked = ref(true)
 const checkedNames = ref([])
+const picked = ref('')
+const selected = ref('')
+const selected2 = ref([])
 </script>
