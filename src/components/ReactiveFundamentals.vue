dc461ab65e8403b0597c78b280c9bb8be0885fee
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 20:35:18 +0700

    New component (ReactiveFundamental)

---
+++ src/components/ReactiveFundamentals.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+}
+</script>
