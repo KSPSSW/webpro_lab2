0fd4623743fce038a9c084147734891f5e5822bf
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 20:50:53 +0700

    New component FormInputBinding

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import ReactiveFundamentals from './components/ReactiveFundamentals.vue'
+import FormInputBinding from './components/FormInputBinding.vue'
 </script>
 
 <template>
+  <FormInputBinding></FormInputBinding>
+  <hr />
   <ReactiveFundamentals></ReactiveFundamentals>
   <hr />
   <div>Hello</div>
