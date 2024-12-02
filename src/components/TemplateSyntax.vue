fd5231a275e5cd625a0796db22191f6519084ba1
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2024-12-02 20:09:02 +0700

    rawHtml

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,3 +1,9 @@
+<script setup>
+var msg = 'Hello Message'
+var rawHtml = '<h1>Hello</h1>'
+</script>
+
 <template>
-  <div>Template Syntax</div>
+  <div>{{ msg }}</div>
+  <div v-html="rawHtml"></div>
 </template>
